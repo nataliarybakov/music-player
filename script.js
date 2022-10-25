8cb703b6364bd14e101862de9d8d1302b611aa6c
Natalia Rybakov <78943679+nataliarybakov@users.noreply.github.com>
2022-10-26 09:48:44 +1100

    JS: the volume slider.

--- script.js
+++ script.js
@@ -42,3 +42,9 @@ function playAudio() {
     function pauseAudio() {
         player.pause();
     }
+
+    const slider = document.getElementById('volumeSlider')
+slider.oninput = function (e) {
+    const volume = e.target.value
+    player.volume = volume;
+};
